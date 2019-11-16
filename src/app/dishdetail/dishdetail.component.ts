31637a6fde1f7846138177158bbf7b6f7f7d0155
sabato.galasso <sabatogalasso90@gmail.com>
2019-11-16 14:14:38 +0100

    Animations Part 1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -6,11 +6,25 @@ import {Location} from "@angular/common";
 import {DishService} from "../services/dish.service";
 import {switchMap} from "rxjs/operators";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
+  animations: [
+    trigger('visibility', [
+      state('shown', style({
+        transform: 'scale(1.0)',
+        opacity: 1
+      })),
+      state('hidden', style({
+        transform: 'scale(0.5)',
+        opacity: 0
+      })),
+      transition('* => *', animate('0.5s ease-in-out'))
+    ])
+  ]
 })
 export class DishdetailComponent implements OnInit {
 
@@ -34,6 +48,9 @@ export class DishdetailComponent implements OnInit {
   ) {
   }
 
+  visibility = 'shown';
+
+
   formErrors = {
     'author': '',
     'comment': ''
@@ -54,12 +71,15 @@ export class DishdetailComponent implements OnInit {
   ngOnInit() {
     this.createForm();
     this.dishService.getDishIds().subscribe(dishIds => this.dishIds = dishIds,errmess => this.errMess = <any>errmess);
-    this.route.params.pipe(switchMap((params: Params) => this.dishService.getDish(params['id'])))
+    this.route.params.pipe(switchMap((params: Params) => {
+      this.visibility = 'hidden';
+      return this.dishService.getDish(params['id']); }))
       .subscribe(dish => {
-        this.dish = dish;
+        this.dish = dish; 
         this.dishcopy = dish;
         this.setPrevNext(dish.id);
-      },errmess => this.errMess = <any>errmess );
+        this.visibility = 'shown'; },
+        errmess => this.errMess = <any>errmess);
   }
 
   goBack(): void {
